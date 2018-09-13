3ef709eaf478215a83368f8782fcef06ce4a7ab9
Nicole <almeida-nicole@live.com>
2018-09-13 16:52:56 -0300

    resolução exercicios

--- funcoes/01.js
+++ funcoes/01.js
@@ -1,23 +1,23 @@
-// hello("what") ->
-// hello() ->
+// hello("what") -> hello word - como esse "what" não define nada, retorna o que está no return
+// hello() -> hello world.
 function hello() {
 	return "Hello, world!"
 }
 
-// show(4) ->
-// show(-4) ->
+// show(4) -> 16. vai ler somente o que o primeiro retorno está chamando// se chamar o segundo return separado = 64
+// show(-4) -> 16, -64 (segundo return separado)
 function show(x) {
-	console.log(`${x} ${x * x}`)
+	console.log(`${x} ${x * x}`) 
 	return x * x
 	console.log(`${x} ${x * x * x}`)
 	return x * x * x
 }
 
-// eq3() ->
-// eq3(1, 2, 3) ->
-// eq3("1", 1, 3) ->
-// eq3("!", "a", "b") ->
-// eq3("!", "!", !) ->
+// eq3() -> retorna 1, pois como não foi definido a, b e c eles se tornam iguais undefined == undefined = true 
+// eq3(1, 2, 3) -> 0
+// eq3("1", 1, 3) -> 0
+// eq3("!", "a", "b") -> 0
+// eq3("!", "!", !) -> não entra na condição, pois ! é um operador.
 function eq3(a, b, c) {
 	if (a === b && a === c) {
 		return 1
@@ -26,20 +26,25 @@ function eq3(a, b, c) {
 	}
 }
 
-// Resultado:
+// Resultado: 9, 11
 function sum(x) {
 	return x + 2
 }
 const x = 5
 console.log(`${sum(x + 2)}, ${sum(sum(x + 2))}`)
 
-// Resultado:
+// Resultado: 26, 12
 function confusion(x, y) {
 	x = 2 * x + y
 	return x
 }
 let x = 2
 let y = 5
-y = confusion(y, x)
-x = confusion(y, x)
+//confusion (5, 2)
+// y= 2 * 5 + 2
+// y = 12
+y = confusion(y, x)  // 12
+// 2 * 12 + 2
+//x = 26
+x = confusion(y, x) // 26
 console.log(`${x}, ${y}`)
--- funcoes/02.js
+++ funcoes/02.js
@@ -6,6 +6,11 @@
 // <objeto>. <nome> decidiu levar
 // <objeto> pra casa."
 
+function story (nome, pronome, objeto) {
+    console.log (`${nome} foi caminhar no parque. ${pronome} encontrou um ${objeto}.
+     ${nome} decidiu levar ${objeto} pra casa.`) 
+}
+// story ("rebeca", "ela", "espelho")
 
 
 
@@ -14,20 +19,47 @@
 // um número qualquer e retorna
 // a raiz quadrada dele.
 
+function raizQuadrada (x) {
 
-
+    return Math.sqrt(x) 
+}
 
 // Escreva uma função em
 // Javascript que recebe um
 // número qualquer e retorna
 // a representação binaria dele.
 
+function binario(x) {
+
+    return x.toString(2)
+}
 
 
 
 // Escreva uma função em Javascript
 // que recebe uma lista de números
 // e retorna o maior número da lista.
+
+function lista (arr) {
+    let sorted = []
+    while (arr.length > 0) {
+        let highest = Infinity
+        let highest_index
+        for (const [index, item] of arr.entries()) {
+			if (item > highest) {
+                highest = item
+				highest_index = index
+			}
+        }
+        sorted.push(highest)
+        arr.splice(highest_index, 1)
+    }
+
+	return highest
+
+}
+
+
 
 
 
@@ -36,7 +68,10 @@
 // que vai checar se duas ou mais strings
 // sao anagramas umas das outras.
 
+function checkAnagrama (string) {
+    if (string.split("").sort().join("") 
 
+}
 
 
 // Escreva uma função em Javascript
